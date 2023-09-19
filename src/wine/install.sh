ae8a1826754a53112417a6f2346bd447dfeda5a5
Maxim Slipenko <no-reply@maxim.slipenko.com>
2023-09-19 13:40:54 +0000

    [wine] try fix winetricks

--- src/wine/install.sh
+++ src/wine/install.sh
@@ -29,7 +29,8 @@ install_debian() {
     telnet \
     cabextract \
     gnupg2 \
-    wget
+    wget \
+    winetricks
 
   wget https://dl.winehq.org/wine-builds/winehq.key -O - | apt-key add -
   echo "deb https://dl.winehq.org/wine-builds/debian $VERSION_CODENAME main" >/etc/apt/sources.list.d/winehq.list
@@ -45,8 +46,8 @@ install_debian() {
   apt-get update
   apt-get install -y --no-install-recommends winehq-staging
 
-  wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -O /usr/bin/winetricks
-  chmod +rx /usr/bin/winetricks
+  # wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -O /usr/bin/winetricks
+  # chmod +rx /usr/bin/winetricks
 
   snippet="export WINEHOME=\"$_REMOTE_USER_HOME\"
 export WINEPREFIX=\"\$WINEHOME/.wine32\"
